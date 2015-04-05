723e2a98732798839cd71b46e549d7497fca5f3f
yuharry <yhannya@gmail.com>
2015-04-06 00:38:16 +0800

    Phrase Chain init!

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+var path = require('path');
+module.exports = function(grunt) {
+    'use strict';
+
+    grunt.initConfig({
+        express: {
+            myServer: {
+                options: {
+                    server: path.resolve(__dirname, 'launch.js'),
+                    hostname: '*',
+                    port: 80
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-express');
+    grunt.registerTask('heroku', ['express:myServer', 'express-keepalive']);
+    grunt.registerTask('default', ['heroku']);
+};
